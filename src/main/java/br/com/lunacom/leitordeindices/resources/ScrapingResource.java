eb7bf40e1fd418eeb04c61e53d927e5f54e3230b
alexpereira2017 <alexpereira2004@gmail.com>
2020-05-14 20:24:59 -0300

    Swagger - Descrições personalizadas para os endpoints

--- src/main/java/br/com/lunacom/leitordeindices/resources/ScrapingResource.java
+++ src/main/java/br/com/lunacom/leitordeindices/resources/ScrapingResource.java
@@ -3,6 +3,7 @@ package br.com.lunacom.leitordeindices.resources;
 import br.com.lunacom.leitordeindices.service.ScrapingHistoricoAtivosService;
 import br.com.lunacom.leitordeindices.service.ScrapingIbovespaService;
 import br.com.lunacom.leitordeindices.util.DataUtil;
+import io.swagger.annotations.ApiOperation;
 import javassist.tools.rmi.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -21,13 +22,14 @@ public class ScrapingResource {
     @Autowired
     ScrapingHistoricoAtivosService scrapingHistoricoAtivosService;
 
+    @ApiOperation(value="Busca as cotações do dia")
     @RequestMapping(value = "ibovespa/carregar-cotacoes-diarias/", method = RequestMethod.GET)
     public ResponseEntity<Void> pesquisar() throws ObjectNotFoundException {
         scrapingService.executar("", new Date());
         return ResponseEntity.ok().build();
     }
 
-
+    @ApiOperation(value="Pesquisa pelo histórico de cada um dos ativos e salva em banco")
     @RequestMapping(value = "historico/ativos/", method = RequestMethod.GET)
     public ResponseEntity<Void> pesquisarHistoricoAtivo(
             @RequestParam("ativos") String ativos,
