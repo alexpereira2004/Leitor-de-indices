d148834ed2f709619f3743ab2a644b9d7010d9e7
alexpereira2017 <alexpereira2004@gmail.com>
2020-06-18 19:02:05 -0300

    Feature: Criacao da entidade para representar os indices Tambem criado o relacionamento entre ativo x indice

--- src/main/java/br/com/lunacom/leitordeindices/domain/Indices.java
+++ src/main/java/br/com/lunacom/leitordeindices/domain/Indices.java
@@ -5,13 +5,12 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.List;
 import java.util.Set;
 
 @Entity
 @Getter
 @Setter
-public class Ativo implements Serializable {
+public class Indices implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -19,21 +18,11 @@ public class Ativo implements Serializable {
     private Integer id;
 
     private String nome;
-    private String nome_completo;
     private String codigo;
     private String tipo;
     private String pais;
     private String caminho;
 
-    @OneToMany(mappedBy = "ativo")
-    private List<Cotacao> cotacoes;
-
-    @ManyToMany
-    @JoinTable(
-            name = "ativo_indice",
-            joinColumns = @JoinColumn(name = "ativo_id"),
-            inverseJoinColumns = @JoinColumn(name = "indice_id"))
-    Set<Indices> indices;
-
-
+    @ManyToMany(mappedBy = "indices")
+    Set<Ativo> ativos;
 }
