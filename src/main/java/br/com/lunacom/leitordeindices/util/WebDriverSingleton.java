c09ec0587d779927a84dac4b695bf92a30513e85
alexpereira2004 <alexpereira2004@gmail.com>
2022-02-04 15:36:26 -0300

    Criacao de Singleton para webdriver Metodo para agrupar as regras de quando criar um novo objeto webdriver - Quando objeto for nulo - Quando navegador tenha sido fechado

--- src/main/java/br/com/lunacom/leitordeindices/util/WebDriverSingleton.java
+++ src/main/java/br/com/lunacom/leitordeindices/util/WebDriverSingleton.java
@@ -4,20 +4,35 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
+import java.util.Objects;
+
 public class WebDriverSingleton {
     private static WebDriver webDriver;
 
     private WebDriverSingleton() {}
 
     public static WebDriver getInstance(Boolean invisivel) {
-        if(webDriver == null) {
-            if (webDriver == null) {
-                FirefoxOptions options = new FirefoxOptions();
-                options.setHeadless(invisivel);
-                webDriver = new FirefoxDriver(options);
-            }
+        if (verificarNovaInstancia()) {
+            FirefoxOptions options = new FirefoxOptions();
+            options.setHeadless(invisivel);
+            webDriver = new FirefoxDriver(options);
         }
         return webDriver;
+    }
+
+    private static boolean verificarNovaInstancia() {
+        boolean response = false;
+        if (Objects.isNull(webDriver)) {
+            response = true;
+        } else {
+            try {
+                webDriver.getCurrentUrl();
+            } catch (Exception e) {
+//                webDriver.quit();
+                response = true;
+            }
+        }
+        return response;
     }
 
 }
