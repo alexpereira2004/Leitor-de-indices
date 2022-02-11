e3835f5f20e6a0d97e5c0737889863869b9cb05b
alexpereira2004 <alexpereira2004@gmail.com>
2022-02-11 19:04:17 -0300

    RabbitMQ Producer - Envia a data da ultima cotacao na mensagem do evento

--- src/main/java/br/com/lunacom/leitordeindices/amq/consumer/RabbitQMConsumer.java
+++ src/main/java/br/com/lunacom/leitordeindices/amq/consumer/RabbitQMConsumer.java
@@ -1,8 +1,12 @@
 package br.com.lunacom.leitordeindices.amq.consumer;
 
 import br.com.lunacom.leitordeindices.amq.producer.ScrapingHistoricoIncidesProducer;
+import br.com.lunacom.leitordeindices.domain.Ativo;
+import br.com.lunacom.leitordeindices.domain.Cotacao;
 import br.com.lunacom.leitordeindices.domain.dto.ResultadoScrapingDto;
 import br.com.lunacom.leitordeindices.domain.message.SolicitacaoScrapingMessage;
+import br.com.lunacom.leitordeindices.service.AtivoService;
+import br.com.lunacom.leitordeindices.service.CotacaoService;
 import br.com.lunacom.leitordeindices.service.Scraping;
 import javassist.tools.rmi.ObjectNotFoundException;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -23,6 +28,12 @@ public class RabbitQMConsumer {
     private Scraping scrapingSiteAdvfnService;
 
     @Autowired
+    private AtivoService ativoService;
+
+    @Autowired
+    private CotacaoService cotacaoService;
+
+    @Autowired
     ScrapingHistoricoIncidesProducer producer;
 
     @RabbitListener(queues = "${rabbitmq.queue.scraping_solicitacao}")
@@ -33,9 +44,10 @@ public class RabbitQMConsumer {
                     new ArrayList<>(Arrays.asList(ativo)),
                     new Date(),false);
             log.info("Message read from Queue : " + solicitacaoScrapingMessage);
+            LocalDateTime ultimaCotacao = determinarUltimaCotacao(ativo);
             final ResultadoScrapingDto dto = ResultadoScrapingDto.builder()
                     .ativo(ativo)
-                    .dia(LocalDateTime.now().toString())
+                    .dia(ultimaCotacao.toString())
                     .build();
             producer.produce(dto);
         } catch (ObjectNotFoundException e) {
@@ -43,5 +55,11 @@ public class RabbitQMConsumer {
         } catch (Exception e) {
             log.error(e.getMessage());
         }
+    }
+
+    private LocalDateTime determinarUltimaCotacao(String ativo) throws ObjectNotFoundException {
+        Ativo obj = ativoService.searchAtivoByCodigo(ativo);
+        Cotacao cotacao = cotacaoService.buscarCotacaoMaisRecente(obj);
+        return cotacao.getReferencia().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
     }
 }
--- src/main/java/br/com/lunacom/leitordeindices/repositories/CotacaoRepository.java
+++ src/main/java/br/com/lunacom/leitordeindices/repositories/CotacaoRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CotacaoRepository extends JpaRepository<Cotacao, Integer> {
@@ -16,4 +17,6 @@ public interface CotacaoRepository extends JpaRepository<Cotacao, Integer> {
     List<Cotacao> findAllByAtivoAndReferenciaAfter(Ativo a, Date d);
     List<Cotacao> findAllByAtivoAndReferencia(Ativo a, Date d);
     List<Cotacao> findAllByAtivoAndReferenciaBetween(Ativo a, Date inicio, Date fim);
+
+    Optional<Cotacao> findTopByAtivoOrderByReferenciaDesc(Ativo integer);
 }
